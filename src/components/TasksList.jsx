1702396f56335d9085241cc7aacd145507b19c95
atieh.zargar <atieh.zargar@partsoftware.com>
2024-12-21 10:37:42 +0330

    feat : add simple data

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,9 +1,20 @@
+"use client";
 
+import { useState } from "react";
 
 export default function Taskslist() {
+  const [tasks, setTasks] = useState([
+    {
+      title: "task1",
+      piority: "high",
+      status: "complete",
+    },
+  ]);
   return (
-  <div className="my-10">
-    task list
-  </div>
+    <div className="my-10">
+      {tasks.map((task, key) => {
+        return <div key={key}>{task.title}</div>;
+      })}
+    </div>
   );
 }
