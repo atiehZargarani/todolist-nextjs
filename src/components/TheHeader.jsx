9b607735f76ef257bcd4e055bcace6258be5468e
atieh.zargar <atieh.zargar@partsoftware.com>
2024-12-21 11:55:51 +0330

    feat : add button for new task

--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -3,10 +3,15 @@ import Image from "next/image";
 export default function TheHeader({title,description}) {
   return (
   
-   <div className="text-center">
+  <div className="flex items-center justify-between px-10">
+     <div className="">
    <h1 className="text-violet-500 font-bold text-lg">{title}</h1>
    <h3 className="text-violet-950 text-sm">{description}</h3>
    </div>
+   <div>
+    <button className="bg-green-800 p-2 rounded-md text-sm">add task</button>
+   </div>
+  </div>
 
   );
 }
