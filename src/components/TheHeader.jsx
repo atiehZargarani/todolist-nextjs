9ce4cd36bf1f6bc2bd6a370923a5bd2cfc0da956
atieh.zargar <atieh.zargar@partsoftware.com>
2024-12-22 12:20:48 +0330

    style : add baclground for header

--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -4,10 +4,16 @@ export default function TheHeader({ title, description, taskList }) {
 
   return (
    
-      <div className="text-center pt-8">
-        <h1 className="text-violet-400 font-bold text-2xl">{title}</h1>
-        <h3 className="text-violet-900 text-sm">{description}</h3>
+    //   <div className="text-center pt-8">
+    //     <h1 className="text-violet-400 font-bold text-2xl">{title}</h1>
+    //     <h3 className="text-violet-900 text-sm">{description}</h3>
     
-    </div>
+    // </div>
+    <div className="text-center bg-violet-800 rounded-t-xl py-3">
+    <h1 className="text-gray-200 font-bold text-2xl">{title}</h1>
+    <h3 className="text-gray-300 text-sm">{description}</h3>
+
+</div>
+
   );
 }
